03b17206f4ca819afd0841f9da526904285735bb
Blueplanetearth <pulkitjain2783@gmail.com>
2022-03-10 16:29:48 +0530

    First Commit

---
+++ demohibernate/src/main/java/com/pullu/demohibernate/App.java
@@ -0,0 +1,41 @@
+package com.pullu.demohibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        
+        Human human = new Human();
+        
+        human.setId(2);
+        human.setName("pj");
+        human.setColor("Blue");
+        
+        
+        Configuration con =  new Configuration().configure().addAnnotatedClass(Human.class);
+            
+        SessionFactory sf = con.buildSessionFactory();
+        
+        Session session = sf.openSession();
+        
+        Transaction tx = session.beginTransaction();
+        
+        session.save(human);
+        
+        tx.commit();
+        
+    }
+}
